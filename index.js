95b32e93f2a1acd34871a22f0ef3c3faa26eba61
al-shr <awebdeff@gmail.com>
2020-02-20 05:57:32 +0300

    create class Programmer

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Programmer {
-  
+  constructor(name) {
+    this.name = name
+
+  }
+
+  code() {
+    console.log(`${this.name} is coding...`)
+  }
 }
+
+class Frontend extends Programmer {
+  angular() {
+    console.log(`${this.name} is creating Angular app..`)
+  }
+}
+
+class Backend extends Programmer {
+  nodejs() {
+    console.log(`${this.name} is programming on Nodejs...`)
+  }
+}
+
+const programmer = new Programmer('Programmer')
+programmer.code()
